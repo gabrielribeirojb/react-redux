ba066a6fa4485b93c0a02b474a49a54dd5bd241c
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-02-05 16:47:33 -0300

    Aula 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import {createStore} from 'redux';
 import './App.css';
+
+const reducer = (state, action) =>{
+  if(action.type === 'INC'){
+    return state + action.payload;
+  }
+  else if(action.type === 'DESC'){
+    return state - action.payload;
+  }
+  return state;
+}
+
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+  console.log("Estado mudou", store.getState());
+})
+
+store.dispatch({type: 'INC', payload:3});
+store.dispatch({type: 'INC', payload:3});
+store.dispatch({type: 'INC', payload:3});
+store.dispatch({type: 'DESC', payload:3});
 
 class App extends React.Component{
 
