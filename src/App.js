2674906a38c8a4b2f8337ba16f231ac29d1c50b6
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-02-05 12:22:38 -0300

    Aula 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {count: 0};
+  }
+
+  increment= () => {
+    this.setState({
+      count: this.state.count +1
+    })
+  }
+
+  decrement= () => {
+    this.setState({
+      count: this.state.count -1
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <button onClick={this.increment}>Incrementar</button>
+        <button onClick={this.decrement}>Decrementar</button>
+        <h2>{this.state.count}</h2>
+      </div>
+    );
+  }
+
 }
 
 export default App;
